a33edbbe2d0e4d97ef147b2686fd41567546157f
William Livernois <willll@uw.edu>
2026-02-27 13:36:25 -0800

    Fixed generator architecture to match seqProp and added tests + docs

--- g3nat/models/__init__.py
+++ g3nat/models/__init__.py
@@ -1,5 +1,5 @@
 from g3nat.models.standard import DNATransportGNN
 from g3nat.models.hamiltonian import DNATransportHamiltonianGNN
-from g3nat.models.generator import DNASequenceGenerator, GeneratorTrainer
+from g3nat.models.generator import SequenceOptimizer
 
-__all__ = ['DNATransportGNN', 'DNATransportHamiltonianGNN', 'DNASequenceGenerator', 'GeneratorTrainer']
+__all__ = ['DNATransportGNN', 'DNATransportHamiltonianGNN', 'SequenceOptimizer']
